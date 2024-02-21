05aa8a3a71e8127d800aa65f254b9e0d99ff133a
Angel Gerardo Molina Valdez <angelgerardomolinavaldez@gmail.com>
2024-02-21 06:31:21 -0600

    add login fixture

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,27 +3,25 @@ from keywords.login_keywords import LoginKeywords
 from keywords.create_new_account_keywords import CreateNewAccountKeywords
 from keywords.account_overview_keywords import AccountOverviewKeywords
 from keywords.transfer_funds_keywords import TransferFundsKeywords
+import pytest
 
-def test_open_new_account(page: Page) -> None:
-    """Test opening a new account in Parabank."""
+@pytest.fixture
+def login(page: Page) -> None:
+    """Fixture to log in to the application."""
     login = LoginKeywords(page)
     login.login_in_to_the_application()
 
+def test_open_new_account(login, page: Page) -> None:
+    """Test opening a new account in Parabank."""
     create_new_account = CreateNewAccountKeywords(page)
     create_new_account.create_new_account("SAVINGS", "13344")
 
-def test_account_overview(page: Page) -> None:
+def test_account_overview(login, page: Page) -> None:
     """Test the account overview page in Parabank."""
-    login = LoginKeywords(page)
-    login.login_in_to_the_application()
-
     account_overview = AccountOverviewKeywords(page)
     account_overview.open_account_overview("13344")
 
-def test_transfer_funds(page: Page) -> None:
+def test_transfer_funds(login, page: Page) -> None:
     """Test transferring funds in Parabank."""
-    login = LoginKeywords(page)
-    login.login_in_to_the_application()
-
     transfer_funds = TransferFundsKeywords(page)
     transfer_funds.transfer_funds("100", "13344", "13344")
